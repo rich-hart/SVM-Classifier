c7a1ae9ee5ec4e64ece3a9881b39624d90d414e3
Rich Hart <RichHOHart@gmail.com>
2013-06-15 12:41:18 -0400

    Generating Stats

--- Chip_Diagnostic.m
+++ Chip_Diagnostic.m
@@ -4,7 +4,7 @@ figure;
 
 clear;
 
-chip_imdata_color = imread('good_chip.jpg');
+chip_imdata_color = imread('bad_chip.jpg');
 
 %ax(1) = subplot(1,2,1);
 
@@ -12,7 +12,7 @@ chip_imdata_color = imread('good_chip.jpg');
 
 chip_imdata_gray=rgb2gray(chip_imdata_color);
 
-chip_edge_data=edge(chip_imdata_gray,'sobel');
+%chip_edge_data=edge(chip_imdata_gray,'sobel');
 
 %ax(2) = subplot(1,2,2);
 
@@ -24,14 +24,32 @@ chip_corner_points=corner(chip_imdata_gray);
 
 %plot(chip_corner_points(:,1), chip_corner_points(:,2), 'r*');
 
+red_data=zeros(length(chip_corner_points),1);
+green_data=zeros(length(chip_corner_points),1);
+blue_data=zeros(length(chip_corner_points),1);
+gray_data=zeros(length(chip_corner_points),1);
 
 for i = 1:length(chip_corner_points)
     y=chip_corner_points(i,1);
     x=chip_corner_points(i,2);
-    chip_imdata_color(x,y,1)=255;
-    chip_imdata_color(x,y,2)=0;
-    chip_imdata_color(x,y,3)=0;
+    red_data(i,1)=chip_imdata_color(x,y,1);
+    green_data(i,1)=chip_imdata_color(x,y,2);
+    blue_data(i,1)=chip_imdata_color(x,y,3);
+    gray_data(i,1)=chip_imdata_gray(x,y);
 end
+
 figure(1);
+
 imshow(chip_imdata_color);
+
+mean(red_data)
+mean(green_data)
+mean(blue_data)
+mean(gray_data)
+
+std(red_data)
+std(green_data)
+std(blue_data)
+mean(gray_data)
+
 %scatter(bad_chip_corner_points(:,2),bad_chip_corner_points(:,1));
