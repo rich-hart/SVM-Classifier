7b8e354d09321ca6bc31dc80a202c9114cc58d59
Rich Hart <RichHOHart@gmail.com>
2013-06-17 21:20:48 -0400

    Final code refactoring for now.  Consider making function more modular in the future

---
+++ Get_Directory_Information.m
@@ -0,0 +1,16 @@
+function [ file_names ] = Get_Directory_Information( dir_name )
+%UNTITLED7 Summary of this function goes here
+%   Detailed explanation goes here
+
+directory_listings = dir(dir_name);
+total_listings=length(directory_listings);
+total_listings=total_listings-2;
+file_names=cell(total_listings,1);
+
+for i = 1 : total_listings
+    file_names{i}=directory_listings(i+2).name;
+end
+
+
+end
+
